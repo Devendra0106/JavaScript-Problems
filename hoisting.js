9f4c2ac4cc8b8cfe495e1a461ec17654b8b60884
Devendra Morankar <devendramorankar@gmail.com>
2023-08-01 01:07:49 +0530

    variable hoisting

--- hoisting.js
+++ hoisting.js
@@ -2,12 +2,13 @@
 //JS mechanism where variables n function declarations are moved to top of their scope before the code execution.
 
 //1. Function Hoisting
+//For each function property created in variable Object which is pointing to that function.
 sum01(2, 7);
 function sum01(a, b) {
     return a + b;
 }
 
-//will be, after creation phase, function declarations are moved to top of their scope
+//After creation phase, function declarations are moved to top of their scope.
 function sum01(a, b) {
     return a + b;
 }
@@ -16,6 +17,13 @@ sum01(2, 7);
 //Hoisting wont work with function expressions & arrow functions
 
 //2. Variable Hoisting
+//For each variable property created in variable Object which is then set to undefined.
+console.log(a); // undefined
+var a;
+a = 10;
 
- 
+//After creation phase, variable declarations are moved to top of their scope and set to undefined.
+var a = undefined;
+console.log(a);
+a = 10;
 
