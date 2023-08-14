c8f89b41c136849c9bb4fb7e8142fcec10adc37f
Devendra Morankar <devendramorankar@gmail.com>
2023-08-14 11:36:02 +0530

    check Anagrams

---
+++ checkAnagrams.js
@@ -0,0 +1,20 @@
+function checkAnagrams(str1, str2) {
+  if(str1.length !== str2.length){
+    return false;
+  }
+  let count = {};
+  for(let c of str1){
+    count[c] = (count[c] || 0) + 1;
+  }
+  console.log(count);
+  for(let c of str2){
+    if(!count[c]){
+      return false;
+    }
+    count[c]--;
+  }
+  console.log(count);
+  return true;
+}
+
+checkAnagrams('ram','mar')
