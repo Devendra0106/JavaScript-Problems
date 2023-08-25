18eaa29b104ec1aacbf41802ec8c3269b5b746b0
Devendra <devendramorankar@gmail.com>
2023-08-25 23:50:43 +0530

    sorting array using recursive function

--- ProblemStatements/recursion_sorting.js
+++ ProblemStatements/recursion_sorting.js
@@ -1,9 +1,30 @@
-let arr = [2, 3, 1, 4];
+let arr = [2, 3, 1, 4, 8, 7, 5, 9, 6];
+let resArray = [];
 let i = 0,
 	j = 1;
+
 function isSorted(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > arr[i + 1]) return false;
+	}
+	return true;
+}
+function sortArray(arr) {
+	if (isSorted(arr)) {
+		resArray = arr;
+		return;
+	}
 	if (arr[i] < arr[j]) {
 		i++;
 		j++;
+		sortArray(arr);
+	} else {
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+		i = 0;
+		j = 1;
+		sortArray(arr);
 	}
 }
+
+sortArray(arr);
+console.log("resArray-->", resArray);
