82e5c19052b80fd1cc6af6321e4929ba972968bc
Devendra <devendramorankar@gmail.com>
2023-09-10 12:12:45 +0530

    msg

--- JsConcepts/promise.js
+++ JsConcepts/promise.js
@@ -1,5 +1,4 @@
 //Promise: It's an object representing eventual completion or failure of asynchronous operation.
-
 let checkNumber = (n) => {
 	return new Promise((resolve, reject) => {
 		if (n > 0) {
@@ -11,22 +10,25 @@ let checkNumber = (n) => {
 };
 
 //A promise is returned object to which we can attach callbacks instead of passing callbacks into a function.
+const success = (res) => {
+	console.log("res-->", res);
+};
+const error = (err) => {
+	console.log("err-->", err);
+};
+checkNumber(10).then(success).catch(error);
 
 // checkNumber(10)
-// 	.then((res) => console.log("res-->", res))
+// 	.then((res) => {
+// 		console.log("res-->", res);
+// 		return checkNumber(20);
+// 	})
+// 	.then((res) => {
+// 		console.log("res-->", res);
+// 		return checkNumber(30);
+// 	})
+// 	.then((res) => {
+// 		console.log("res-->", res);
+// 		return checkNumber(-40);
+// 	})
 // 	.catch((err) => console.log("err-->", err));
-
-checkNumber(10)
-	.then((res) => {
-		console.log("res-->", res);
-		return checkNumber(20);
-	})
-	.then((res) => {
-		console.log("res-->", res);
-		return checkNumber(30);
-	})
-	.then((res) => {
-		console.log("res-->", res);
-		return checkNumber(-40);
-	})
-	.catch((err) => console.log("err-->", err));
