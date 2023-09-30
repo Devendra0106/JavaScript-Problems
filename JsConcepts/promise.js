a7a9ec22e6a9464745949870f9f99eaf4f316273
Devendra <devendramorankar@gmail.com>
2023-09-30 12:12:45 +0530

    arrow function as a callback to promise

--- JsConcepts/promise.js
+++ JsConcepts/promise.js
@@ -1,4 +1,5 @@
 //Promise: It's an object representing eventual completion or failure of asynchronous operation.
+
 let checkNumber = (n) => {
 	return new Promise((resolve, reject) => {
 		if (n > 0) {
@@ -10,13 +11,10 @@ let checkNumber = (n) => {
 };
 
 //A promise is returned object to which we can attach callbacks instead of passing callbacks into a function.
-const success = (res) => {
-	console.log("res-->", res);
-};
-const error = (err) => {
-	console.log("err-->", err);
-};
-checkNumber(10).then(success).catch(error);
+
+checkNumber(10)
+	.then((res) => console.log("res-->", res))
+	.catch((err) => console.log("err-->", err));
 
 // checkNumber(10)
 // 	.then((res) => {
