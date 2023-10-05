a75b2b8bb5c8ebdb8b57b44bc96eaef4e3c8673d
Devendra <devendramorankar@gmail.com>
2023-10-05 12:12:45 +0530

    more on destructuring

--- JsConcepts/destructuring.js
+++ JsConcepts/destructuring.js
@@ -1,5 +1,29 @@
-let a = [1, 2, 3, (5, 5, 7), (3, 8, 9)];
+//Array destructuring
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let [a, b, ...c] = arr;
+console.log(a, b, c); // 1, 2, [ 3, 4, 5, 6, 7 ]
 
-let [x, y, ...z] = a;
+//Object destructuring
+let obj = {
+	fName: "Dev",
+	add: "India",
+	num: "10001001",
+};
+let { add, fName } = obj;
+console.log(fName, add);
 
+//Params destructing
+let emp = {
+	fName: "John",
+	lName: "Smith",
+	add: "India",
+};
+function empDetails({ fName, add }) {
+	return `${fName} is from ${add}`;
+}
+console.log(empDetails(emp));
+
+//Tricky one
+let arrr = [1, 2, 3, (5, 5, 7), (3, 8, 9)];
+let [x, y, ...z] = arrr;
 console.log(z);
