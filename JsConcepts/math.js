386abf00a715b8d999b412b5fae0e73cc42334fd
Devendra <devendramorankar@gmail.com>
2023-10-16 12:12:45 +0530

    math functions

---
+++ JsConcepts/math.js
@@ -0,0 +1,9 @@
+console.log(Math.ceil(2.8));
+
+console.log(Math.floor(2.8));
+
+console.log(Math.pow(5, 3));
+
+console.log(Math.max(4, 7, 1, 2, 9, 33, 91, 75, 98));
+
+console.log(Math.floor(Math.random() * 10) + 1); // Generates random number between 1 to 10
