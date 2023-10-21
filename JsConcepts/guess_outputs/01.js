5ad4e54f3ad417fa449208d402eb49eb8b4a609a
Devendra <devendramorankar@gmail.com>
2023-10-21 12:12:45 +0530

    find output

--- JsConcepts/guess_outputs/01.js
+++ JsConcepts/guess_outputs/01.js
@@ -1,2 +1,10 @@
 var a = (b = c = 10);
 console.log(a, b, c); // 10 10 10
+
+// In strict mode b can't be accessed outside closure, without strict mode it will be accessed.
+(function () {
+	var a = (b = 3); // evaluates to b = 3; and var a = b.
+})();
+
+// b = 3;
+// var a = b;
