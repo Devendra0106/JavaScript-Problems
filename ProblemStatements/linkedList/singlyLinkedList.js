fe581354915a60377e61dd7a88f48a51183dac0f
Devendra <devendramorankar@gmail.com>
2023-11-17 11:34:10 +0530

    linked list class

---
+++ ProblemStatements/linkedList/singlyLinkedList.js
@@ -0,0 +1,15 @@
+//Node
+class Node {
+	constructor(val) {
+		this.val = val; //piece of data - val
+		this.next = null; //reference to next node - next
+	}
+}
+
+class SinglyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+}
