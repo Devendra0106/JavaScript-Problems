b7f4948692fc300df0816694d29607ec6909f095
Devendra <devendramorankar@gmail.com>
2023-11-21 10:34:15 +0530

    classes n hoisting

---
+++ JsConcepts/guess_outputs/08.js
@@ -0,0 +1,12 @@
+var instance = new Example();
+class Example {
+	constructor() {
+		this.value = 42;
+	}
+}
+console.log(instance.value);
+
+// o/p--> This will result in a ReferenceError.
+// Explanation--> Unlike function declarations, class declarations are not hoisted.
+// The Example class is not available until after its declaration.
+// Calling the constructor before the class is defined results in a ReferenceError.
