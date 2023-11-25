1c325ca2c1fdbb18035d418f1112a49f771cb458
Devendra <devendramorankar@gmail.com>
2023-11-25 12:36:17 +0530

    push in linked list

--- ProblemStatements/linkedList/singlyLinkedList.js
+++ ProblemStatements/linkedList/singlyLinkedList.js
@@ -12,4 +12,23 @@ class SinglyLinkedList {
 		this.tail = null;
 		this.length = 0;
 	}
+	push(val) {
+		var newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	}
 }
+
+let list = new SinglyLinkedList();
+list.push("Hi");
+list.push("LL");
+list.push("welcome!");
+list.push("ok!");
+console.log(list);
