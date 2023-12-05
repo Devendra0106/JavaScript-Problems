05c65e71beb9b6047c8a696a32935eefcaedbe73
Devendra <devendramorankar@gmail.com>
2023-12-05 06:33:45 +0530

    reverse linked list

--- ProblemStatements/linkedList/singlyLinkedList.js
+++ ProblemStatements/linkedList/singlyLinkedList.js
@@ -130,6 +130,21 @@ class SinglyLinkedList {
 		}
 		console.log("Linked List-->", arr);
 	}
+
+	reverse() {
+		let node = this.head;
+		this.head = this.tail;
+		this.tail = node;
+		let next;
+		let prev = null;
+		for (let i = 0; i < this.length; i++) {
+			next = node.next;
+			node.next = prev;
+			prev = node;
+			node = next;
+		}
+		return this;
+	}
 }
 
 let list = new SinglyLinkedList();
@@ -142,7 +157,8 @@ list.push("welcome!");
 // console.log(list.get(1));
 // list.set(2, "Linked List");
 // console.log(list.get(1));
-console.log(list.insert(0, "Dev"));
-console.log(list.remove(3));
-console.log(list);
+// console.log(list.insert(0, "Dev"));
+// console.log(list.remove(3));
+list.print();
+list.reverse();
 list.print();
