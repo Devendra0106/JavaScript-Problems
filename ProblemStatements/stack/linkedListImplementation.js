e14b9c3e4fe4f478d259d4a0a50581223e0f5cd7
Devendra <devendramorankar@gmail.com>
2023-12-06 14:36:17 +0530

    push in stack

---
+++ ProblemStatements/stack/linkedListImplementation.js
@@ -0,0 +1,33 @@
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+
+//Stack Data Structure
+class Stack {
+	constructor() {
+		this.first = null; //First element in stack
+		this.last = null; //Last element in stack
+		this.size = 0; //Size of Stack
+	}
+
+	push(val) {
+		let newNode = new Node(val);
+		if (!this.first) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			let temp = this.first;
+			this.first = newNode;
+			this.first.next = temp;
+		}
+		return ++this.size;
+	}
+}
+
+let stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
