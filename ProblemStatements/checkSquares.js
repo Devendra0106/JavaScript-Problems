2cc1bd205695087952d3ee6178bcc04d27d31b45
Devendra <devendramorankar@gmail.com>
2023-12-27 12:10:15 +0530

    check squares in another arr, O(n)

--- ProblemStatements/checkSquares.js
+++ ProblemStatements/checkSquares.js
@@ -1,17 +1,45 @@
 //O(n^2)
+// function checkSquares(arr1, arr2) {
+// 	if (arr1.length !== arr2.length) {
+// 		return false;
+// 	}
+// 	for (const num of arr1) {
+// 		let indexOfSquareNum = arr2.indexOf(num ** 2);
+// 		if (indexOfSquareNum === -1) {
+// 			return false;
+// 		}
+// 		arr2.splice(indexOfSquareNum, 1);
+// 	}
+// 	return true;
+// }
+
+//O(n)
 function checkSquares(arr1, arr2) {
-	if (arr1.length !== arr2.length) {
-		return false;
-	}
+	let numbersMap = {};
+	let squaresMap = {};
 	for (const num of arr1) {
-		let indexOfSquareNum = arr2.indexOf(num ** 2);
-		if (indexOfSquareNum === -1) {
+		numbersMap[num] = (numbersMap[num] || 0) + 1;
+	}
+
+	for (const sqr of arr2) {
+		squaresMap[sqr] = (squaresMap[sqr] || 0) + 1;
+	}
+	//Returns false when square is not there in 2nd arr.
+	for (const num in numbersMap) {
+		if (!squaresMap[num ** 2]) {
 			return false;
 		}
-		arr2.splice(indexOfSquareNum, 1);
 	}
+
+	//Returns false when count of squares doesn't match.
+	for (const num in numbersMap) {
+		if (numbersMap[num] !== squaresMap[num ** 2]) {
+			return false;
+		}
+	}
+
 	return true;
 }
 
-let res = checkSquares([2, 4, 1, 3], [1, 11, 16, 4]);
+let res = checkSquares([2, 1, 3, 2], [1, 4, 4, 9]);
 console.log(res);
