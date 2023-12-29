7b2412f562dc57b206941082a51ef7ccf9f1bef6
Devendra <devendramorankar@gmail.com>
2023-12-30 00:28:38 +0530

    count unique numbers

--- ProblemStatements/countUniqueNums.js
+++ ProblemStatements/countUniqueNums.js
@@ -1,14 +1,17 @@
 function countUniqueNums(arr) {
-  let i=0, j=1;
-  while (j<=arr.length) {
-    if(arr[i] !== arr[j]){
-      i++;
-      arr[i] = arr[j];
-    }
-    j++;
-  }
-  return i;
+	if (arr.length === 0) return 0;
+	let i = 0,
+		j = 1;
+	while (j <= arr.length) {
+		if (arr[i] !== arr[j]) {
+			i++;
+			arr[i] = arr[j];
+		}
+		j++;
+	}
+	return i;
 }
-                                
-let arr = [1,1,2,2,3,4,4,5,6,6,7,8]; //input array is sorted
-countUniqueNums(arr);
+
+let arr = [1, 1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8]; //input array is sorted
+let res = countUniqueNums(arr);
+console.log("Res-->", res);
