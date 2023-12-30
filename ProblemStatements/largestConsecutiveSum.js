038d77c684a9f73084a0bf0dfe3a53993b6cb52e
Devendra <devendramorankar@gmail.com>
2023-12-30 13:18:01 +0530

    sum of n consecutive numbers

--- ProblemStatements/largestConsecutiveSum.js
+++ ProblemStatements/largestConsecutiveSum.js
@@ -1,16 +1,19 @@
+//O(n)      --> Best case
+//O(n^2)    --> Worst case
 function largestSum(arr, num) {
-    let max = 0, temp = 0, c = 0;
-    for (let i = 0; i < arr.length - num + 1; i++) {
-        temp = 0;
-        for (let j = 0; j < num; j++) {
-            temp += arr[i+j];
-        }
-        if(max < temp){
-            c++;
-            max = temp;
-        }
-    }
-    return max;
+	let max = 0,
+		temp = 0;
+	for (let i = 0; i <= arr.length - num; i++) {
+		temp = 0;
+		for (let j = 0; j < num; j++) {
+			temp += arr[i + j];
+		}
+		if (max < temp) {
+			max = temp;
+		}
+	}
+	return max;
 }
 
-const largeSum = largestSum([1,2,3,4,6,7,8,4,3,5], 4);
+const largeSum = largestSum([1, 2, 3, 4, 6, 7, 8, 4, 3, 5], 4);
+console.log("Res-->", largeSum);
