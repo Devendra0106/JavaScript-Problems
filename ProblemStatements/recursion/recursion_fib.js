8d9ca3f5e46f477cfe37383365e545d85e74ac3b
Devendra <devendramorankar@gmail.com>
2024-01-01 22:32:06 +0530

    alternative

--- ProblemStatements/recursion/recursion_fib.js
+++ ProblemStatements/recursion/recursion_fib.js
@@ -4,5 +4,11 @@ function fib(num) {
 	return fib(num - 1) + fib(num - 2);
 }
 
+//or
+// function fib(n) {
+// 	if (n <= 2) return 1;
+// 	return fib(n - 1) + fib(n - 2);
+// }
+
 let res = fib(10);
 console.log("Res-->", res);
