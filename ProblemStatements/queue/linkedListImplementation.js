a3a404c3f3b5d27c433b98437e16bca8f3ba2372
Devendra <devendramorankar@gmail.com>
2024-01-15 13:28:41 +0530

    enqueue in Queue

---
+++ ProblemStatements/queue/linkedListImplementation.js
@@ -0,0 +1,31 @@
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+
+class Queue {
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
+
+	enqueue(val) {
+		let newNode = new Node(val);
+		if (!this.first) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			this.last.next = newNode;
+			this.last = newNode;
+		}
+		return ++this.size;
+	}
+}
+
+let q = new Queue();
+console.log(q.enqueue(1));
+console.log(q.enqueue(20));
+console.log(q.enqueue(30));
