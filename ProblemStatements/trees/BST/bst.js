524cff25571868f44079f5c0ca4f777c042c89d9
Devendra <devendramorankar@gmail.com>
2024-02-05 22:43:12 +0530

    BFS

--- ProblemStatements/trees/BST/bst.js
+++ ProblemStatements/trees/BST/bst.js
@@ -52,6 +52,21 @@ class BinarySearchTree {
 		if (!found) return null;
 		return current;
 	}
+
+	//Breadth First Search
+	BFS() {
+		let node = this.root,
+			queue = [],
+			data = [];
+		queue.push(node);
+		while (queue.length) {
+			node = queue.shift();
+			data.push(node);
+			if (node.left) queue.push(node.left);
+			if (node.right) queue.push(node.right);
+		}
+		return data;
+	}
 }
 
 // 			 10
